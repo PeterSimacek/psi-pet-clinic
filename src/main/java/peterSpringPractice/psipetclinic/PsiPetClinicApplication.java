3d2c7c3e06fc59ff4958e233758e3689a37fd9c7
Peter Simacek <psi@codemanufaktur.com>
2020-05-27 15:22:59 +0200

    first commit of all the initial files of the project: .gitignore application.properties maven-wrapper.jar maven-wrapper.properties mvnw mvnw.cmd pom.xml PsiPetClinicApplication.java PsiPetClinicApplicationTests.java

---
+++ src/main/java/peterSpringPractice/psipetclinic/PsiPetClinicApplication.java
@@ -0,0 +1,13 @@
+package peterSpringPractice.psipetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsiPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsiPetClinicApplication.class, args);
+	}
+
+}
